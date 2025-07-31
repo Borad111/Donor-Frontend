5022fa8026c9853d7f44a2ae8e02fea47a0181d0
Borad111 <meetpatel39501010@gmail.com>
2025-07-31 09:30:53 +0530

    Routes.ts File Created

---
+++ constants/routes.ts
@@ -0,0 +1,12 @@
+// You can customize the names as per your project
+export const ROUTES = {
+    ABOUT_US: '/about-us',
+    ITEMS: '/items',
+    TICKETS: '/tickets',
+    SPONSORSHIPS: '/tickets', // if same as tickets
+    PREVIEW: '/comming',
+    DONATE_ITEM: '/donate-Items',
+    LOGIN: '/login',
+    MY_INFO: '/my-Info',
+    FORGOT:'/forgot',
+};
