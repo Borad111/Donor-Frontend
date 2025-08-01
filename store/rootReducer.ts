8731d9f5d45bf2c74871efdf2691be14120ac54d
Mr. Jay Patel <108916822+Jay-Vekariya@users.noreply.github.com>
2025-08-01 12:52:52 +0530

    Dynamic Footer Component

--- store/rootReducer.ts
+++ store/rootReducer.ts
@@ -1,8 +1,11 @@
 import { mySettingApi } from "@/api/mySetting/mySettingApi";
+import { featureItemApi } from "@/features/about-us/api/featureItemApi";
+
 import { combineReducers } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
     [mySettingApi.reducerPath]:mySettingApi.reducer,
+    [featureItemApi.reducerPath]:featureItemApi.reducer,
 })
 
 export default rootReducer;
